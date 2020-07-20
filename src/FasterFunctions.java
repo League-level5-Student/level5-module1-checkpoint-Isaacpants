8247215ccef39f507931e9e73ad58d9a20f5a19a
dave <dave@DESKTOP-5BF6RM1>
2020-07-20 16:18:46 -0700

    finnishd moduile 1

--- src/FasterFunctions.java
+++ src/FasterFunctions.java
@@ -1,17 +1,36 @@
+import java.util.Random;
 
 public class FasterFunctions {
 	public static void main(String[] args) {
 		float totalTime = 0;
 		long startTime = System.currentTimeMillis();
-		//Your Code Here
-		Thread t = new Thread(() ->{
+		// Your Code Here
+		Thread t1 = new Thread(() -> {
+			SlowFunctions.slowSortLargeArray();
+		});
+		Thread t2 = new Thread(() -> {
+			SlowFunctions.ackermann(3, 14);
 			
 		});
-		
-		
-		
-		float endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
+		Thread t3 = new Thread(() -> {
+SlowFunctions.millionsOfSqrts();
+		});
+		t1.start();
+		t2.start();
+		t3.start();
+		try {
+			t1.join();
+			t2.join();
+			t3.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		float endTime = (float) ((double) System.currentTimeMillis() - (double) startTime) / 1000.0f;
 		totalTime += endTime;
 		System.out.println("Total Time: " + totalTime);
+		
+		
+		
 	}
 }
